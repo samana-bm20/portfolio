773595da6159401f0612f8da0f6c1ee4fadd36cc
Samana Mirza <samana.butool.mirza10@gmail.com>
2025-01-20 10:40:26 +0530

    nav padding and bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ export default {
     colors: {
       primary: "#66fcf1",
       secondary: "#c5c6c7",
-      bgblack: "#11171d",
-      bggray: "#090f14", //#080b0e
+      bgblack: "#0c1821",
+      bggray: "#142735", //#080b0e
     },
     listStyleType: {
       square: "square",
